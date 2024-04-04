defbe7d0fb7ec431415f6ed9cbf7c25d35bb6058
baharezgui <bahaeddinerezgui29@gmail.com>
2024-04-04 13:51:02 +0100

    final commit

--- script.js
+++ script.js
@@ -4,23 +4,27 @@ var qty = document.querySelectorAll(".qty")
 var price = document.querySelectorAll(".price")
 var total_price = document.querySelector("#total-price")
 var deleteItem = document.querySelectorAll(".bi-trash3")
-var like = document.querySelectorAll("bi-heart-fill")
+var like = document.querySelectorAll(".bi-heart-fill")
 
 //console.log(deleteItem)
 
-// Initialize total price 
+// Initial Total Price 
+
 
 function calcTotal() {
     var qty = document.querySelectorAll(".qty")
     var price = document.querySelectorAll(".price")
     var total = 0;
-    qty.forEach(function(quantity, i){
+    qty.forEach(function (quantity, i) {
         total += parseInt(quantity.innerHTML) * parseInt(price[i].innerHTML)
         console.log(quantity)
     })
     total_price.innerHTML = total
 }
 calcTotal()
+
+
+
 
 let plusAdded = false;
 if (!plusAdded){
@@ -29,8 +33,38 @@ plus.forEach(function(item, i){
         console.log("Plus button clicked");
        let quantity = parseInt(qty[i].innerHTML);
        qty[i].innerHTML = parseInt(qty[i].innerHTML) + 1;
-      
+      calcTotal()
 })
 })
 }
 
+mins.forEach(function (item, i) {
+    item.addEventListener("click", function () {
+        if (parseInt(qty[i].innerHTML) === 1) {
+            return false
+        } else {
+
+            qty[i].innerHTML = parseInt(qty[i].innerHTML) - 1
+            calcTotal()
+        }
+    })
+})
+deleteItem.forEach(function (deleteBtn, i) {
+    deleteBtn.addEventListener("click", function () {
+        deleteBtn.parentNode.parentNode.remove()
+        calcTotal()
+    })
+})
+
+like.forEach(function (likeBtn) {
+    likeBtn.addEventListener("click", function (event) {
+event.stopPropagation();
+        if (likeBtn.style.color === "red") {
+            likeBtn.style.color = ""
+        } else {
+
+            likeBtn.style.color = "red"
+        }
+    })
+})
+// console.log(price)
